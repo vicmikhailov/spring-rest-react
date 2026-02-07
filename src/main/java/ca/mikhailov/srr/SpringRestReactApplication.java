41bc08b296adec545237cafc51a05716490b626d
Victor Mikhailov <victor@mikhailov.ca>
2026-02-07 11:34:08 -0500

    feat: Add GEMINI.md with project overview

---
+++ src/main/java/ca/mikhailov/srr/SpringRestReactApplication.java
@@ -0,0 +1,13 @@
+package ca.mikhailov.srr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestReactApplication.class, args);
+	}
+
+}
